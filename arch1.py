5de79dfe2bc852c3effaf55fd2bf583198f3f4e2
freemso <freemso@hotmail.com>
2017-06-04 16:36:50 +0800

    finish arch1

---
+++ arch1.py
@@ -0,0 +1,239 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Separate hidden layers and convolution layers of questions and answers
+"""
+import logging
+import pathlib
+
+import jieba
+import numpy as np
+from keras.engine import Input, Model
+from keras.layers import Embedding, Dense, Activation, Conv1D, MaxPooling1D, concatenate, Flatten, K, merge, dot
+from keras.preprocessing import sequence
+from pattern3.vector import l2_norm
+from scipy.sparse.linalg.isolve.tests.test_iterative import params
+
+import qa
+import w2v
+import util.ProgressBar
+
+__author__ = "freemso"
+
+MODEL_WEIGHT_FILE = "model/arch1.model"
+TRAIN_DATA_FILE = "data/BoP2017_DBAQ_dev_train_data/BoP2017-DBQA.train.txt"
+DEV_DATA_FILE = "data/BoP2017_DBAQ_dev_train_data/BoP2017-DBQA.dev.txt"
+
+SENTENCE_LEN = 50
+HIDDEN_LAYER_DIM = 200
+NUM_FILTERS = 1000  # number of filters for each filter size
+FILTER_SIZES = [2, 3, 4]  # kinds of filters
+MARGIN = 0.009
+LEARNING_RATE = 0.01
+
+NUM_EPOCH = 10
+BATCH_SIZE = 10
+
+
+class Arch1(object):
+    def __init__(self):
+        self.w2v = w2v.WordVector()
+
+        self.model = self._build()
+
+        model_weight_file = pathlib.Path(MODEL_WEIGHT_FILE)
+        if model_weight_file.is_file():
+            logging.info("Loading model weight...")
+            self.model.load_weights(MODEL_WEIGHT_FILE)
+        else:
+            logging.info("Training model...")
+            # Preprocess train data
+            qa_items = preprocess(TRAIN_DATA_FILE)
+
+            # Train the model
+            self._train(qa_items)
+
+    def _train(self, qa_items):
+        questions = []
+        pos_answers = []
+        neg_answers = []
+        bar = util.ProgressBar.ProgressBar(total=len(qa_items))
+        for item in qa_items:
+            bar.log()
+            question = item.question
+
+            question_seg = jieba.cut(question)
+            question_word_idx = []
+            for word in question_seg:
+                if word in self.w2v.wv:
+                    question_word_idx.append(self.w2v.word2idx[word] + 1)
+                else:
+                    question_word_idx.append(0)
+
+            for pos_answer in item.pos_answers:
+                pos_answer_seg = jieba.cut(pos_answer)
+                pos_answer_word_idx = []
+                for word in pos_answer_seg:
+                    if word in self.w2v.wv:
+                        pos_answer_word_idx.append(self.w2v.word2idx[word] + 1)
+                    else:
+                        pos_answer_word_idx.append(0)
+
+                for neg_answer in item.neg_answers:
+                    neg_answer_seg = jieba.cut(neg_answer)
+                    neg_answer_word_idx = []
+                    for word in neg_answer_seg:
+                        if word in self.w2v.wv:
+                            neg_answer_word_idx.append(self.w2v.word2idx[word] + 1)
+                        else:
+                            neg_answer_word_idx.append(0)
+                    questions.append(question_word_idx)
+                    pos_answers.append(pos_answer_word_idx)
+                    neg_answers.append(neg_answer_word_idx)
+
+        questions = sequence.pad_sequences(questions, maxlen=SENTENCE_LEN, padding="post", truncating="post", value=0)
+        pos_answers = sequence.pad_sequences(pos_answers, maxlen=SENTENCE_LEN, padding="post", truncating="post", value=0)
+        neg_answers = sequence.pad_sequences(neg_answers, maxlen=SENTENCE_LEN, padding="post", truncating="post", value=0)
+
+        # Train model
+        self.model.fit({"question_input": questions,
+                        "answer_pos_input": pos_answers,
+                        "answer_neg_input": neg_answers},
+                       {"pos_sim_output": np.ones(questions.shape),
+                        "neg_sim_output": -np.ones(questions.shape)},
+                       epochs=NUM_EPOCH, batch_size=BATCH_SIZE)
+
+        # save model weights
+        self.model.save_weights(MODEL_WEIGHT_FILE, overwrite=True)
+
+    def _build(self):
+        """ To setup the structure of the network """
+        # embedding layer weight setting
+        embedding_weights = np.zeros((self.w2v.vocab_size + 1, self.w2v.vector_size))
+        embedding_weights[0, :] = np.zeros((self.w2v.vector_size,))
+        for index, word in self.w2v.idx2word.items():
+            embedding_weights[index + 1, :] = self.w2v.wv[word]
+
+        # Network structure
+        logging.info("Building model...")
+        embedding_layer = Embedding(self.w2v.vocab_size + 1, self.w2v.vector_size,
+                                    weights=[embedding_weights], input_length=SENTENCE_LEN,
+                                    trainable=False, name="emb")
+
+        hidden_layer_q = Dense(HIDDEN_LAYER_DIM, use_bias=True, name="question_hidden")
+        hidden_layer_a = Dense(HIDDEN_LAYER_DIM, use_bias=True, name="answer_hidden")
+
+        q_in = Input(shape=(SENTENCE_LEN,), name="question_input")
+        a_pos_in = Input(shape=(SENTENCE_LEN,), name="answer_pos_input")
+        a_neg_in = Input(shape=(SENTENCE_LEN,), name="answer_neg_input")
+
+        q = embedding_layer(q_in)
+        a_pos = embedding_layer(a_pos_in)
+        a_neg = embedding_layer(a_neg_in)
+
+        q = hidden_layer_q(q)
+        a_pos = hidden_layer_a(a_pos)
+        a_neg = hidden_layer_a(a_neg)
+
+        concat_q = []
+        concat_a_pos = []
+        concat_a_neg = []
+        for sz in FILTER_SIZES:
+            cnn_q = Conv1D(NUM_FILTERS, sz, strides=1, name="question_conv_" + str(sz))
+            cnn_a = Conv1D(NUM_FILTERS, sz, strides=1, name="answer_conv_" + str(sz))
+
+            pooling_q = MaxPooling1D(pool_size=SENTENCE_LEN - sz + 1, name="question_pool_" + str(sz))
+            pooling_a = MaxPooling1D(pool_size=SENTENCE_LEN - sz + 1, name="answer_pool_" + str(sz))
+
+            q_cnn = cnn_q(q)
+            a_pos_cnn = cnn_a(a_pos)
+            a_neg_cnn = cnn_a(a_neg)
+
+            q_pool = pooling_q(q_cnn)
+            a_pos_pool = pooling_a(a_pos_cnn)
+            a_neg_pool = pooling_a(a_neg_cnn)
+
+            concat_q.append(q_pool)
+            concat_a_pos.append(a_pos_pool)
+            concat_a_neg.append(a_neg_pool)
+        q = Activation(activation="tanh", name="question_tanh")(Flatten()(concatenate(concat_q, axis=-1)))
+        a_pos = Activation(activation="tanh", name="answer_pos_tanh")(Flatten()(concatenate(concat_a_pos, axis=-1)))
+        a_neg = Activation(activation="tanh", name="answer_neg_tanh")(Flatten()(concatenate(concat_a_neg, axis=-1)))
+
+        pos_sim = merge([q, a_pos], mode=cosine, output_shape=lambda x: x[:-1], name="pos_sim_output")
+        neg_sim = merge([q, a_neg], mode=cosine, output_shape=lambda x: x[:-1], name="neg_sim_output")
+
+        model = Model(inputs=[q_in, a_pos_in, a_neg_in], outputs=[pos_sim, neg_sim])
+
+        # compile model
+        model.compile(loss=max_margin_loss,
+                      optimizer="sgd",
+                      metrics=['accuracy'])
+
+        return model
+
+
+def max_margin_loss(y_true, y_pred):
+    signed = y_pred * y_true  # we do this, just so that y_true is part of the computational graph
+    pos = signed[0::2]
+    neg = signed[1::2]
+    # negative samples are multiplied by -1, so that the sign in the rankSVM objective is flipped below
+    return K.max(K.maximum(0, MARGIN - pos - neg))
+
+
+def cosine(x):
+    return dot(x, -1, normalize=True)
+
+
+def polynomial(x):
+    return (params['gamma'] * dot(x[0], x[1]) + params['c']) ** params['d']
+
+
+def sigmoid(x):
+    return K.tanh(params['gamma'] * dot(x[0], x[1]) + params['c'])
+
+
+def rbf(x):
+    return K.exp(-1 * params['gamma'] * l2_norm(x[0] - x[1]) ** 2)
+
+
+def euclidean(x):
+    return 1 / (1 + l2_norm(x[0] - x[1]))
+
+
+def exponential(x):
+    return K.exp(-1 * params['gamma'] * l2_norm(x[0] - x[1]))
+
+
+def gesd(x):
+    euclidean = 1 / (1 + l2_norm(x[0] - x[1]))
+    sigmoid = 1 / (1 + K.exp(-1 * params['gamma'] * (dot(x[0], x[1]) + params['c'])))
+    return euclidean * sigmoid
+
+
+def aesd(x):
+    euclidean = 0.5 / (1 + l2_norm(x[0] - x[1]))
+    sigmoid = 0.5 / (1 + K.exp(-1 * params['gamma'] * (dot(x[0], x[1]) + params['c'])))
+    return euclidean + sigmoid
+
+
+def preprocess(file_path):
+    q2qa = {}
+    with open(file_path, encoding="utf-8-sig") as in_file:
+        for line in in_file:
+            label, question, answer = line.strip().split("\t")
+            if question not in q2qa.keys():
+                q2qa[question] = qa.QAItem(question)
+            q2qa[question].add_ans(answer, bool(int(label)))
+    return q2qa.values()
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    arch1 = Arch1()
+
+
+if __name__ == '__main__':
+    main()
---
+++ qa.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Question and answer pair structure
+"""
+import logging
+
+__author__ = "freemso"
+
+
+class QAItem(object):
+    def __init__(self, question):
+        self.pos_answers = []
+        self.question = question
+        self.neg_answers = []
+
+    def add_ans(self, answer, is_pos):
+        if is_pos:
+            self.pos_answers.append(answer)
+        else:
+            self.neg_answers.append(answer)
